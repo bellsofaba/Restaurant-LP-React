dba70df130c352551a59d4fa4e7e12e11202e177
bellsofaba <bellsofaba@gmail.com>
2023-10-05 05:44:20 -0400

    Copy Edits

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -14,11 +14,11 @@ const Home = () => {
         </div>
         <div className="home-text-section">
           <h1 className="primary-heading">
-            Your Favourite Food Delivered Hot & Fresh
+            Fresh sumptuousness for you to relish 
           </h1>
           <p className="primary-text">
-            Healthy switcher chefs do all the prep work, like peeding, chopping
-            & marinating, so you can cook a fresh food.
+            All the prep work done on your behalf; peeling, chopping
+            & marinating, so you can put it all together and watch it cook.
           </p>
           <button className="secondary-button">
             Order Now <FiArrowRight />{" "}
